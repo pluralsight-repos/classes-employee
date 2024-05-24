16ff5c783270d03caaa7433d56ffc18253b31eb0
msg4edson <msg.edson@gmail.com>
2024-05-24 06:29:32 -0300

    Overriding parent class instance methods

--- classes.py
+++ classes.py
@@ -1,43 +1,27 @@
 class Employee:
-    def __init__(self, name, age, position, salary):
+    def __init__(self, name, age, salary):
         self.name = name
         self.age = age
-        self.position = position
         self.salary = salary
-        self._annual_salary = None
 
     def increase_salary(self, percent):
         self.salary += self.salary * (percent/100)
 
-    def __str__(self):
-        return f"{self.name} is {self.age} year old. Employee is a {self.position} with the salary of ${self.salary}"
-    
-    def __repr__(self):
-        return (
-            f"Employee("
-            f"{repr(self.name)},{repr(self.age)},{repr(self.position)},{repr(self.salary)})"
-        )
-    
-    @property
-    def salary(self):
-        return self._salary
-        
-    @salary.setter
-    def salary(self, salary):
-        if salary < 1000:
-            raise ValueError('Minimum wage is $1000')
-        self._annual_salary = None
-        self._salary = salary # no public
+class Tester(Employee):
+    def run_tests(self):
+        print(f"Testing is started by {self.name}...")
+        print("Tests are done!")
 
-    @property
-    def annual_salary(self):
-        if self._annual_salary is None:
-            self._annual_salary = self.salary * 12
-        return self._annual_salary      
+class Developer(Employee):
+    def increase_salary(self, percent, bonus=0):
+        self.salary += self.salary * (percent/100)
+        self.salary += bonus
 
-employee1 = Employee("Jin-Soo", 38, "developer", 1200)
-employee2 = Employee("Lauren", 44, "tester", 1000)
+employee1 = Tester("Lauren", 44, 1000)
+employee2 = Developer("Ji-Soo", 38, 1000)
 
-print("Before update salary",employee1.annual_salary)
-employee1.salary = 1000
-print("After update salary", employee1.annual_salary)
+employee1.increase_salary(20)
+employee2.increase_salary(20,30)
+
+print(employee1.salary)
+print(employee2.salary)
